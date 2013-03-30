023293f8424332268afc6a7aa8bef75f3cc4ccc9
Jude Aakjaer <santouras@gmail.com>
2013-03-30 19:01:13 +1100

    add in first user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  points     :float
+#  password_digest :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  remember_token  :string(255)
+#
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(
+      name: "Example",
+      email: "foo@bar.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    );
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+
+  it { should be_valid }
 end
