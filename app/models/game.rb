03737d0ccb58c81a33f9796f61c39550ce4dfb9b
Jude Aakjaer <santouras@gmail.com>
2013-03-31 14:33:09 +1100

    add in user1_id and user2_id as accessable attributes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ActiveRecord::Base
+  attr_accessible :points, :score1, :score2, :user1_id, :user2_id
+
   belongs_to :user1, :class_name => 'User'
   belongs_to :user2, :class_name => 'User'
-  attr_accessible :points, :score1, :score2
 end
