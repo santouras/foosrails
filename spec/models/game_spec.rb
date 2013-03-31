201e90a15f1ff487733933f237e070f34e450046
Jude Aakjaer <santouras@gmail.com>
2013-03-31 14:33:48 +1100

    initial game spec. check for properties

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,38 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  user1_id   :integer
+#  user2_id   :integer
+#  score1     :integer
+#  score2     :integer
+#  points     :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    @game = Game.new(
+      user1_id: user,
+      user2_id: user.dup,
+      score1: 10,
+      score2: 4
+    );
+  end
+
+  subject { @game }
+
+  it { should respond_to(:user1_id) }
+  it { should respond_to(:user2_id) }
+  it { should respond_to(:score1) }
+  it { should respond_to(:score2) }
+  it { should respond_to(:points) }
+
+
 end
