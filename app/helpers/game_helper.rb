93490acf7be5e8ea202cbcddb3b2e08ae3914ecc
Jude Aakjaer <santouras@gmail.com>
2013-04-23 21:44:27 +1000

    GameHelper#G,diff methods added

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -3,4 +3,24 @@ module GameHelper
   def calculate value
 
   end
+
+  def G
+    case diff
+    when 0, 1
+      1
+    when 2
+      3 / 2.0
+    else
+      (11 + diff ) / 8.0
+    end
+  end
+
+  private
+
+  def diff
+    @diff = self.score1 - self.score2
+
+    @diff *= -1 if @diff < 0
+    @diff
+  end
 end
