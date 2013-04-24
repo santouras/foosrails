7d5df00a91275654059f4d998dc9a6e9674e9c3e
Jude Aakjaer <santouras@gmail.com>
2013-04-24 11:38:40 +1000

    Game spec wrap validation context

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -14,37 +14,39 @@
 
 require 'spec_helper'
 
-describe Game do
-  let(:user1) { FactoryGirl.create(:user) }
-  let(:user2) { FactoryGirl.create(:user) }
+describe "Game" do
+  context "validation" do
+    let(:user1) { FactoryGirl.create(:user) }
+    let(:user2) { FactoryGirl.create(:user) }
 
-  before do
+    before do
 
-    @game = Game.new(
-      user1: user1,
-      user2: user2,
-      score1: 10,
-      score2: 4,
-      weight: 10
-    );
-  end
+      @game = Game.new(
+        user1: user1,
+        user2: user2,
+        score1: 10,
+        score2: 4,
+        weight: 10
+      );
+    end
 
-  subject { @game }
+    subject { @game }
 
-  it { should respond_to(:user1) }
-  it { should respond_to(:user2) }
-  it { should respond_to(:score1) }
-  it { should respond_to(:score2) }
-  it { should respond_to(:points) }
-  it { should respond_to(:weight) }
+    it { should respond_to(:user1) }
+    it { should respond_to(:user2) }
+    it { should respond_to(:score1) }
+    it { should respond_to(:score2) }
+    it { should respond_to(:points) }
+    it { should respond_to(:weight) }
 
-  context "with no user" do
-    before { @game.user1_id = nil }
-    it { should_not be_valid }
-  end
+    context "with no user" do
+      before { @game.user1_id = nil }
+      it { should_not be_valid }
+    end
 
-  context "with a bad score" do
-    before { @game.points = -4 }
-    it { should_not be_valid }
+    context "with a bad score" do
+      before { @game.points = -4 }
+      it { should_not be_valid }
+    end
   end
 end
