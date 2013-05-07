08c6bddda12ff2f573cc2f1b06153ecaa6103ddb
Jude Aakjaer <santouras@gmail.com>
2013-05-07 23:47:13 +1000

    change to cookie_store for rails 4 rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Foosrails::Application.config.session_store :encrypted_cookie_store, key: '_foosrails_session'
+Foosrails::Application.config.session_store :cookie_store, key: '_foosrails_session'
