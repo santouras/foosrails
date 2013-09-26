35e181f78a636a3237e93598e9af31a00dfc5d5c
Jude Aakjaer <santouras@gmail.com>
2013-09-26 11:28:23 +0200

    change to symbol reference

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,10 @@
 class Game < ActiveRecord::Base
-  belongs_to :user1, :class_name => 'User'
-  belongs_to :user2, :class_name => 'User'
+  belongs_to :user1, class_name: 'User'
+  belongs_to :user2, class_name: 'User'
 
   validates :user1_id, presence: true
   validates :user2_id, presence: true
-  validates :points, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :points, numericality: { greater_than_or_equal_to: 0 }
 
   before_create :calculate
 
