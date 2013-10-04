14e8950ce7dfddfba4b506e1f5b813862ad0d077
Jude Aakjaer <santouras@gmail.com>
2013-10-04 11:48:47 +0200

    add routes for sessions, static and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Foosrails::Application.routes.draw do
   resources :users
+  resources :games
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new', via: [:get, :post]
+  match '/signin', to: 'sessions#new', via: [:get, :post]
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
