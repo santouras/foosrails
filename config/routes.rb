139660ee63323166391b0ebbf7b8a0584a090a21
Jude Aakjaer <santouras@gmail.com>
2013-10-07 17:52:21 +0200

    match -> get in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Foosrails::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/signup', to: 'users#new', via: [:get, :post]
-  match '/signin', to: 'sessions#new', via: [:get, :post]
-  match '/signout', to: 'sessions#destroy', via: :delete
+  get '/signup', to: 'users#new', via: [:get, :post]
+  get '/signin', to: 'sessions#new', via: [:get, :post]
+  get '/signout', to: 'sessions#destroy', via: :delete
 end
